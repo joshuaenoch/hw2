137a6cf6a79e448bb0866c1158cc6ff50c99b2ba
Joshua <joshuaechow@gmail.com>
2025-01-29 13:13:51 -0800

    What we did last time

---
+++ Untitled-1.py
@@ -0,0 +1,64 @@
+# 1. Write a function to calculate impurity (e.g., Shannon entropy or Gini index).
+# 2. Implement a recursive function to split data based on impurity reduction and grow the tree.
+# 3. Set stopping conditions to prevent infinite recursion (e.g., max depth, min samples per split, pure nodes).
+from collections import Counter
+class DecisionTree:
+    def __init__(self, data):
+        self.head = Node
+        self.data = data
+        pass
+
+    def gini(self,n, total):
+        return 1 - ((n / total) ** 2)
+
+    def gini_all(self, options):
+         order = options
+         for i in range(len(options)):
+             order[i] = gini(options[i].n, options[i].total)
+         return order
+
+    # anthony is going to finish this but its complete  rn
+    def why_did_I_do_this(self):
+        self.ginivalues = {}
+        ans = 1
+        counter = Counter(self.data)
+        for x in counter:
+            ans = ans -(x / len(co)nter)
+*
+
+
+
+
+    def split(self, node, node_options):
+        if node.isleaf():
+            return
+        else:
+            # get the best option
+            splitting_on = gini_all(node_options)
+            # remove that option
+            node_options = node_options.remove(splitting_on)
+            # grow on left and split on it
+            node.left = Node(values=splitting_on.options[0])
+            split(node.left, node_options)
+            # grow on right and split on it
+            node.left = Node(values=splitting_on.options[1])
+            split(node.right, node_options)
+
+
+    def isleaf():
+        pass
+
+
+class Node():
+    def __init__(self, feature=None, threshold=None, left=None, right=None, values=None, options=None, gini=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.values = values
+        self.options = options
+        self.gini = gini
+
+    def dealWithNonBinary(self, ):
+
+
